ae08d46773c9bcdacc48ab6e5ac8ad39a664bab9
snehakayala <snehakayala06@gmail.com>
2021-04-25 10:23:43 +0530

    mul

--- project.py
+++ project.py
@@ -1,9 +1,13 @@
-s = input()
+s = input('enter:')
 if s=="comboproject":
     print(s)
 else:
     print("thanga")
 #adding two numbers
 def add(a,b):
-    return a,b
+    return a+b
 print(add(5,10))
+
+def mul(a,b):
+    return a*b
+print(mul(6,9))
